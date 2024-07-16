a6281961a29a5957425b95f7ea1f8909e69a1313
khulyso <khulyso.dev@gmail.com>
2024-07-16 14:32:04 +0200

    files updated

--- script.js
+++ script.js
@@ -29,6 +29,26 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     };
 
+    ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+    ScrollReveal().reveal('.home-img, .services-container, .contact form', {origin:'bottom'});
+    ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
+    ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
+
+
+    const typed = new Typed('.multiple-text', {
+        strings: ['Developers', 'Tutors', 'Electricians'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
      
 
 
